409c3028985b8e7a0e4f9c0126d484796afac90f
Bratu <alex_bratu24@yahoo.com>
2016-01-10 14:14:26 +0200

    Added application in manifest
    Push to app from parse working

--- app/src/main/java/application/SimplePushApplication.java
+++ app/src/main/java/application/SimplePushApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.parse.Parse;
 import com.parse.ParseInstallation;
+import com.parse.ParsePush;
 
 /**
  * Created by Alexandru on 1/10/2016.
@@ -15,5 +16,6 @@ public class SimplePushApplication extends Application{
         super.onCreate();
         Parse.initialize(this, "fR37pLYx7thotUP606CIiu3efypK3U0otl5F0Won", "C0iZxMhVdhsru8CyjJgJE4AyuozOWgG760D8fodI");
         ParseInstallation.getCurrentInstallation().saveInBackground();
+        ParsePush.subscribeInBackground("Munti");
     }
 }
--- app/src/main/java/com/example/alex/simplepush/MainActivity.java
+++ app/src/main/java/com/example/alex/simplepush/MainActivity.java
@@ -5,8 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import com.parse.Parse;
-import com.parse.ParseInstallation;
 import com.parse.ParseObject;
 import com.parse.ParsePush;
 

d1775a365fac019854ea918fb35c10aefbd2b64a
alexmuntean14 <alexmuntean14@gmail.com>
2016-01-10 16:38:06 +0200

    LightOn
    butonul care trimite obiect pt bec

--- app/src/main/java/com/example/alex/simplepush/MainActivity.java
+++ app/src/main/java/com/example/alex/simplepush/MainActivity.java
@@ -9,13 +9,14 @@ import com.parse.ParseObject;
 import com.parse.ParsePush;
 
 public class MainActivity extends AppCompatActivity  {
-
-  private Button sendPush;
+    public int mod=0;
+  private Button sendPush,lightOn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sendPush= (Button)findViewById(R.id.buttonPush);
+        lightOn = (Button) findViewById(R.id.buttonBec);
 
 
         sendPush.setOnClickListener(new View.OnClickListener() {
@@ -25,9 +26,31 @@ public class MainActivity extends AppCompatActivity  {
                 push.setChannel("Munti");
                 push.setMessage("Bratu....");
                 push.sendInBackground();
-                ParseObject testObject = new ParseObject("TestObject");
-                testObject.put("foo", "bar");
+                //ParseObject testObject = new ParseObject("TestObject");
+              //  testObject.put("foo", "bar");
+              //  testObject.saveInBackground();
+            }
+        });
+
+        lightOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ParseObject testObject = new ParseObject("ModBec");
+                testObject.put("cod", "03");
+                if(mod==0) {
+                    testObject.put("mod", "on");
+                    lightOn.setText("Stringe bec");
+                    mod=1;
+                }
+                else
+                {
+                    testObject.put("mod", "off");
+                    lightOn.setText("Aprinde bec");
+                    mod=1;
+                }
                 testObject.saveInBackground();
+
             }
         });
 
